8c3a68ebfdb57b8c99f2fe650da59972a9cce922
Alex Symonds <alex-symonds@gmail.com>
2021-11-15 17:02:28 +0000

    CSS added to documents, items and start of price check section on Job page

--- adminas/static/adminas/job_price_check_btn.js
+++ adminas/static/adminas/job_price_check_btn.js
@@ -28,18 +28,24 @@ function toggle_price_check(e){
 
 
 function update_price_check_on_page(btn, current_status){
-
-    price_check_div = btn.parentElement;
-    p = price_check_div.querySelector('p');
-
+    
     btn.dataset.current_status = current_status;
     if(current_status){
-        console.log('Confirmed / wrong');
-        p.innerHTML = 'Confirmed';
-        btn.innerHTML = 'Wrong Price';
+        btn.innerHTML = 'confirmed';
+        var wanted_class = 'on';
+        var unwanted_class = 'off';
     } else {
-        console.log('UNCONFIRMED / confirm');
-        p.innerHTML = 'UNCONFIRMED';
-        btn.innerHTML = 'Confirm Price';
+        btn.innerHTML = 'UNCONFIRMED';
+        var unwanted_class = 'on';
+        var wanted_class = 'off';
+    }
+
+    let price_check_div = btn.parentElement;
+    if(price_check_div.classList.contains(unwanted_class)){
+        price_check_div.classList.remove(unwanted_class);
+    }
+
+    if(!price_check_div.classList.contains(wanted_class)){
+        price_check_div.classList.add(wanted_class);
     }
 }
